0e56931f73a8d609eeda41468e6a2014a7f354cb
Georg Osang <gosang@ist.ac.at>
2015-10-26 15:44:38 +0100

    code for median

---
+++ examples/test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+ 
+im1 = cv2.imread('sample_images/04.ppm')
+im2 = cv2.imread('sample_images/08.ppm')
+im3 = cv2.imread('sample_images/12.ppm')
+med = cv2.imread('sample_images/12.ppm')
+# here we overwrite the array 'med' to maintain the data type uint instead of float
+np.median(np.array([im1, im2, im3]), 0, out=med)
+cv2.imwrite('outmed.jpg', med)
+im = cv2.subtract(im2, med)
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+imgray = cv2.GaussianBlur(imgray,(19,19),0)
+ret,thresh = cv2.threshold(imgray,32,255,0)
+contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(im,contours,-1,(0,255,0),3)
+print len(contours)
+cv2.imwrite('out.jpg', im)
