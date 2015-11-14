32040854a02627f3c865f46a7e82fe66ddd80eec
Georg Osang <gosang@ist.ac.at>
2015-11-14 10:23:42 +0100

    curve detection code

--- examples/python/run.py
+++ examples/python/run.py
@@ -3,7 +3,15 @@ from anki import *
 import sysv_ipc
 import numpy as np
 import cv2
-from time import sleep,clock
+from time import sleep,time
+
+# angle between two vectors, output in [0, pi]
+def angle(a, b = (1,0)):
+    return np.arccos(np.dot(a,b) / np.linalg.norm(a) * np.linalg.norm(b))
+
+def dprint(text):
+    #pass
+    print(text)
 
 RHO = "E6:D8:52:F1:D9:43"
 BOSON = "D9:81:41:5C:D4:31"
@@ -22,15 +30,17 @@ if err:
     print "Couldn't connect, code", err
     raise SystemExit
 
-status = car.set_speed(1200, 5000)
-if status:
-    print "Couldn't set speed, code",  status
-    raise SystemExit
-
 # attach to shared memory with key 192012003
 S=sysv_ipc.SharedMemory(key=192012003)
 
 sleep(1.5)
+
+
+
+status = car.set_speed(1200, 5000)
+if status:
+    print "Couldn't set speed, code",  status
+    raise SystemExit
 
 
 ims = []
@@ -60,13 +70,22 @@ startx = 1696/2
 starty = 720/2
 
 # number of laps before quitting
-NUM_LAPS = 20
+NUM_LAPS = 10
 # laps already driven
 laps = 0
 
-t_start = time.clock()
+t_start = time()
 t_lapstart = t_start
 laptimes = []
+
+#velocities for the last few frames, first one is newest
+NUM_VELS = 10
+velocities = [(0,0)]*NUM_VELS
+positions = [(0,0)]*NUM_VELS
+is_curvy = [False]*NUM_VELS
+
+# positions where track changes from curvy to straight or vice versa
+landmarks = []
 
 # number of times we didn't find any contour
 bad = 0
@@ -100,13 +119,13 @@ while (True):
     # TODO: take elapsed time into account and scale vector accordingly
     expected_x = old_x + (old_x - oldold_x)
     expected_y = old_y + (old_y - oldold_y)
-    print ("Expected position: %4d, %4d" % (expected_x, expected_y))
+d    print ("Expected position: %4d, %4d" % (expected_x, expected_y))
 
     if hierarchy == None:
         # no contours found at all
-        print("No car found")
+        dprint("No car found")
         cv2.imwrite('out%s.jpg' % bad, im)
-        bad += 1
+        bad = (bad + 1) % MAX_IMAGES
         # use expected position if no car found
         xpos = expected_x
         ypos = expected_y
@@ -121,15 +140,19 @@ while (True):
                 M = cv2.moments(contours[i])
                 xpos = int(M['m10']/M['m00'])
                 ypos = int(M['m01']/M['m00'])
-                print("Actual position:   %4d, %4d" % (xpos, ypos))
+                dprint("Actual position:   %4d, %4d" % (xpos, ypos))
                 centroids.append((xpos, ypos))
                 homology_cars += 1
         if homology_cars >= 2:
+            cv2.imwrite('out%s.jpg' % bad, im)
+            bad = (bad + 1) % MAX_IMAGES
             # TODO: What happens if we find two cars?
             pass
         if homology_cars == 0:
             # TODO: find a good blob that might be our car instead of dead reckoning
             # TODO: use expected position and maybe convexity defects for this
+            cv2.imwrite('out%s.jpg' % bad, im)
+            bad = (bad + 1) % MAX_IMAGES
             # use expected position if no homology car found
             xpos = expected_x
             ypos = expected_y
@@ -137,9 +160,34 @@ while (True):
     #cv2.imwrite('out%s.jpg' % i, im)
     #sleep(0.1)
 
-    t_end = time.clock()
+
+    # now x_pos and y_pos are the official positions
+    velocities = [(xpos - old_x, ypos - old_y)] + velocities[0:NUM_VELS-1]
+    positions = [(xpos, ypos)] + positions[0:NUM_VELS-1]
+    # average the last two vectors, and the previous two vectors
+    # and for each of them compute the angle of the resulting vector
+    angle_new = angle(np.add(velocities[1], velocities[0]))
+    angle_old = angle(np.add(velocities[3], velocities[2]))
+    # compute the difference between the angles, centered around 0
+    diff = (angle_new - angle_old + np.pi) % (2*np.pi) - np.pi
+
+    if np.abs(diff) < 0.05:
+        is_curvy = [False] + is_curvy[0:NUM_VELS-1]
+        # straight
+    else:
+        is_curvy = [True] + is_curvy[0:NUM_VELS-1]
+        # curvy
+
+    dprint(angle_new, angle_old, is_curvy[0], positions[0])
+
+    if (is_curvy[0] and is_curvy[1] and not is_curvy[2] and not is_curvy[3])
+        or (if not is_curvy[0] and not is_curvy[1] and is_curvy[2] and is_curvy[3]):
+        landmarks.append(positions[2])
+
+
+    t_end = time()
     dt = t_end - t_start
-    print("Elapsed time:    %f seconds\n" % dt)
+    dprint("Elapsed time:    %f seconds\n" % dt)
     t_start = t_end
 
     if ypos < starty and xpos >= startx and old_x < startx:
@@ -161,6 +209,8 @@ while (True):
         if laps == NUM_LAPS:
             # stop after 20 laps
             break
+    
+    sleep(0.1)
 
 
 print(laptimes)
